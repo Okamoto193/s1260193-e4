3b3791df74edde32ce8a87cb80fda6c3e586627b
Okamoto193 <52599221+Okamoto193@users.noreply.github.com>
2019-07-06 22:10:36 +0900

    Dice Game

---
+++ dice.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class dice {
+	Random rand = new Random();
+
+	private int dice1;
+	private int dice2;
+
+	public dice() {
+		this.dice1 = rand.nextInt(5)+1;
+		this.dice2 = rand.nextInt(5)+1;
+	}
+
+	int value() {
+		return dice1 + dice2;
+	}
+
+	public static void main(String[] args) {
+		dice dice = new dice();
+
+		System.out.println("Rolling the dice...");
+		System.out.println("Die 1:  " + dice.dice1);
+		System.out.println("Die 1:  " + dice.dice2);
+		System.out.println("Total value:  " + dice.value());
+	}
+}
